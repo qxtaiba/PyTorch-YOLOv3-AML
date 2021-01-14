a3385c9ff7d84a31f202e47a64f8657f26d8d1a4
qxtaiba <qan205@nyu.edu>
2021-01-14 19:12:13 +0400

    removed uneccessary activation functions

--- models.py
+++ models.py
@@ -41,10 +41,7 @@ def create_modules(module_defs, img_size, cfg):
 
             if mdef['activation'] == 'leaky':  # activation study https://github.com/ultralytics/yolov3/issues/441
                 modules.add_module('activation', nn.LeakyReLU(0.1, inplace=True))
-            elif mdef['activation'] == 'swish':
-                modules.add_module('activation', Swish())
-            elif mdef['activation'] == 'mish':
-                modules.add_module('activation', Mish())
+            
 
         elif mdef['type'] == 'BatchNorm2d':
             filters = output_filters[-1]
